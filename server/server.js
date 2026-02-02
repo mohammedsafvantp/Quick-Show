142bd515fa55c92961611bcf218f81e4be797baa
mohammedsafvantp <mohammedsafvantp@gmail.com>
2026-02-02 19:16:53 +0530

    moviewebsite

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
-import connectDB from './config/db.js';
+import connectDB from './configs/db.js';
 import { clerkMiddleware } from '@clerk/express'
 import { inngest, functions } from "./inngest/index.js"
 import { serve } from "inngest/express";
